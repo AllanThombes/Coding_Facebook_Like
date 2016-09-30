1007ca00cd251429b6360e32f335a4d8dbd0f7dc
thomas <thomas1.leblan@coding-academy.fr>
2016-09-30 15:36:24 +0200

    Replace CDN by bower install for materialize

--- stalker/server/models/message.js
+++ stalker/server/models/message.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports = mongoose.model('message', {
-    title: { type: String, default: '', required: true },
     text: { type: String, default: '', required: true },
     createdAt: {type: Date, default: Date.now, required: true},
-    authorId: {type: Schema.Types.ObjectId, required: true}
+    authorId: {type: Schema.Types.ObjectId, required: true},
+    updatedAt:{type: Date, default: ''}
 });
